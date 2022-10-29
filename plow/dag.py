a2d02562855a9bc31c98b06d749500a9d5cae377
Vitor Lopes Maia Albuquerque <vlma@cin.ufpe.br>
2022-10-29 15:40:32 -0400

    parsing yaml file

--- plow/dag.py
+++ plow/dag.py
@@ -1,4 +1,6 @@
-from typing import Any, Callable, List, Optional
+import yaml
+from pathlib import Path
+from typing import Any, Callable, List, Optional, Union
 from pydantic import BaseModel
 
 StepSchema = Any ## TODO: Generate NodeType Union
@@ -7,7 +9,6 @@ class Dag(BaseModel):
     name: str
     inputs: Any
     steps: List[StepSchema]
-    # TODO: fill the return type of the dag run
 
     def run(self, inputs: dict[str, Any]) -> Any:
         ...
@@ -16,5 +17,13 @@ class Dag(BaseModel):
         ...
 
 
-def make_dag(yaml_str: Optional[str] = None) -> Dag:
-    ...
+def make_dag(*, yaml_path: Optional[Union[Path, str]] = None, yaml_str: Optional[str] = None) -> Dag:
+    assert yaml_str or yaml_path, "yaml_str or yaml_path is required"
+    yaml_values = {}
+    if yaml_str:
+        yaml_values = yaml.unsafe_load(yaml_str)
+    elif yaml_path:
+        with open(yaml_path, 'rb') as f:
+            yaml_values = yaml.unsafe_load(f)
+    return Dag(**yaml_values)
+
