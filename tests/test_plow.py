dc26bc361c88c55cf395559c25772e19b3d8b9ee
Vitor Lopes Maia Albuquerque <vlma@cin.ufpe.br>
2022-10-29 16:50:45 -0400

    fix typo

--- tests/test_plow.py
+++ tests/test_plow.py
@@ -4,7 +4,7 @@ from plow.dag import Dag
 
 def test_dag(quadratic_solver_tasks: None, quadratic_solver_yaml_fixture: str):
     dag = plow.make_dag(yaml_str=quadratic_solver_yaml_fixture)
-    assert isinstance(dag, Dag), "make_dag failes to create Dag with valid yaml"
+    assert isinstance(dag, Dag), "make_dag fails to create Dag with valid yaml"
     result = dict(dag.run(inputs={"a": -4, "b": 2, "c": 6}))
     assert result["root_1"] == -1
     assert result["root_2"] == 1.5
